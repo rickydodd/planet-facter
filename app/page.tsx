bcadaee9140234d986a162d19eff0c5828800683
Ricky Dodd <63101193+rickydodd@users.noreply.github.com>
2024-11-22 22:23:35 +1100

    add: initial planet state, json parsing

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,42 @@
+'use client'
+
+import React from 'react'
+import Header from '../components/Header'
+import Planet from '../components/Planet'
+
+const initialState = {
+  data: {},
+  planetData: {},
+  position: 0,
+  planetNames: [],
+}
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'set_initial':
+      return {
+        ...state,
+        data: action.data,
+        planetNames: action.data.reduce(
+          (planetNames, planet) => [...planetNames, planet.name],
+          [],
+        ),
+      }
+    default:
+      throw new Error(`Unexpected type passed to dispatch. Received: ${type}`)
+  }
+}
+
 export default function Home() {
+  const [state, dispatch] = React.useReducer(reducer, initialState)
+
+  React.useEffect(() => {
+    fetch(process.cwd() + 'data.json')
+      .then((res) => res.json())
+      .then((data) => {
+        dispatch({ type: 'set_initial', data })
+      })
+  }, [])
+
   return <div></div>
 }
