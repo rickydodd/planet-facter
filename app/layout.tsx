43da02c36c5a6d9736fe6295e852e4e92488d9af
rickydodd <rickydodd@users.noreply.github.com>
2024-11-27 06:21:20 +0000

    chore: run prettier

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,28 @@
-import type { Metadata } from 'next'
-import localFont from 'next/font/local'
-import './globals.css'
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
 
 const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-  weight: '100 900',
-})
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
 const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-  weight: '100 900',
-})
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
 
 export const metadata: Metadata = {
-  title: 'Planet Facter',
+  title: "Planet Facter",
   description:
-    'Planet Facter is a planet facts web app, providing interesting facts on all planets in the Solar System.',
-}
+    "Planet Facter is a planet facts web app, providing interesting facts on all planets in the Solar System.",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
@@ -32,5 +32,5 @@ export default function RootLayout({
         {children}
       </body>
     </html>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,44 +1,44 @@
-'use client'
+"use client";
 
-import React from 'react'
-import Header from '../components/Header'
-import Planet from '../components/Planet'
+import React from "react";
+import Header from "../components/Header";
+import Planet from "../components/Planet";
 
 const initialState = {
   data: [],
   position: null,
-}
+};
 
 const reducer = (state, action) => {
   switch (action.type) {
-    case 'set_initial':
+    case "set_initial":
       return {
         ...state,
         data: action.data,
         position: action.data.length > 0 ? 0 : null,
-      }
-    case 'set_position':
+      };
+    case "set_position":
       return {
         ...state,
         position: action.position,
-      }
+      };
     default:
       throw new Error(
         `Unexpected type passed to dispatch. Received: ${action.type}`,
-      )
+      );
   }
-}
+};
 
 export default function Home() {
-  const [state, dispatch] = React.useReducer(reducer, initialState)
+  const [state, dispatch] = React.useReducer(reducer, initialState);
 
   React.useEffect(() => {
-    fetch(process.cwd() + 'data.json')
+    fetch(process.cwd() + "data.json")
       .then((res) => res.json())
       .then((data) => {
-        dispatch({ type: 'set_initial', data })
-      })
-  }, [])
+        dispatch({ type: "set_initial", data });
+      });
+  }, []);
 
-  return <div></div>
+  return <div></div>;
 }
