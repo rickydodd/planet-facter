e7580e6b0cb084bdc2c3eadc966f6ef9d0b80f3e
Ricky Dodd <63101193+rickydodd@users.noreply.github.com>
2024-11-27 16:16:48 +1100

    add: fonts 'Antonio' and 'League Spartan'

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,15 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Antonio, League_Spartan } from "next/font/google";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+const antonio = Antonio({
+  variable: "--font-antonio",
+  subsets: ["latin"],
 });
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+
+const spartan = League_Spartan({
+  variable: "--font-spartan",
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
@@ -26,9 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${spartan.variable} ${antonio.variable} antialiased`}>
         {children}
       </body>
     </html>
